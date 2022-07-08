158084418d59ec479a4fc438ac1217fbe76fed8b
DESKTOP-EK8DN2N\Acryl <dawson@acryl.ai>
2022-07-08 13:58:02 +0900

    스크립트 수정

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const puppeteer = require('puppeteer');
 (async () => {
 	const browser = await puppeteer.launch(); // 크로미움 실행
 	const page = await browser.newPage(); // 크로미움의 page 객체 생성
-	await page.setViewport({ width: 800, height: 400 }); // page의 크기
+	await page.setViewport({ width: 900, height: 400 }); // page의 크기
 
 	await page.goto('https://ssl.jobcan.jp/login/pc-employee-global'); // 이동할 위치 : 잡캔의 로그인 주소
 	await page.evaluate(() => {
-		document.querySelector('input#client_id').value = '회사명'; // 회사명을 입력
+		document.querySelector('input#client_id').value = 'iacryl'; // 회사명을 입력
 		document.querySelector('input#email').value = '아이디'; // 아이디를 입력
 		document.querySelector('input#password').value = '비밀번호'; // 비밀번호를 입력
 	});
@@ -20,11 +20,14 @@ const puppeteer = require('puppeteer');
 		&from%5By%5D=${firstDay.year}&from%5Bm%5D=${firstDay.month}&from%5Bd%5D=${firstDay.day}
 		&to%5By%5D=${lastDay.year}&to%5Bm%5D=${lastDay.month}&to%5Bd%5D=${lastDay.day}&type=pdf`;
 
+	console.log(link)
+
 	await page.goto(link);
 	await page.waitForSelector('#search-result > div.table-responsive.text-nowrap > table'); // 테이블 요소가 로딩될 때까지 대기
 
 	await page.evaluate(_ => {
-		window.scrollBy(0, 1970); // 테이블의 위치까지 스크롤
+		const target = document.querySelector("#search-result > div.table-responsive.text-nowrap > table").getBoundingClientRect().y;
+		window.scrollBy(0, target-80); // 테이블의 위치까지 스크롤
 	});
 
 	await page.screenshot({path: 'screenshot.png'}); // 스샷찍어서 screenshot.png로 저장
